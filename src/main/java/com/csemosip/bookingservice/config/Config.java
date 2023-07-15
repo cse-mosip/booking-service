5c43004ef7a93dd9a430b95081a5e50d6765ca61
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2023-07-16 01:17:32 +0530

    Added cross origin configuration globally

--- src/main/java/com/csemosip/bookingservice/config/Config.java
+++ src/main/java/com/csemosip/bookingservice/config/Config.java
@@ -3,11 +3,24 @@ package com.csemosip.bookingservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.modelmapper.ModelMapper;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
+            }
+        };
     }
 }
