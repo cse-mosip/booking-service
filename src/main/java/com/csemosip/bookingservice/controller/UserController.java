166769aad94a6257ecb7572b8e4f18ba6adf7fa0
Nimantha Cooray <nimanthacooray@gmail.com>
2023-07-17 23:52:40 +0530

    Protect recently merged routes - GET resources/{id}/available - GET /user/{id}, POST /user/register - PATCH /bookings/{id}/status

--- src/main/java/com/csemosip/bookingservice/controller/UserController.java
+++ src/main/java/com/csemosip/bookingservice/controller/UserController.java
@@ -7,6 +7,7 @@ import com.csemosip.bookingservice.service.Impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -18,12 +19,14 @@ public class UserController extends AbstractController{
     UserServiceImpl userService;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Map<String, Object>> findUser(@PathVariable String id) {
         User user = userService.findUser(id);
         return sendSuccessResponse(user, HttpStatus.OK);
     }
 
     @PostMapping("/register")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Map<String, Object>> createUser(@RequestBody UserDTO userDTO) {
         User user = userService.createUser(userDTO);
         return sendSuccessResponse(user, HttpStatus.CREATED);
