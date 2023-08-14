eea9f4dd0c8b23f1c63f5331723d50423b4c38a9
Nethum Lamahewage <nethumpinsara@gmail.com>
2023-08-14 19:07:32 +0530

    Fix new user login
    `RESOURCE_USER` is the default user role, so we need to assign that to
    any user that is not in the database

--- src/main/java/com/csemosip/bookingservice/controller/UserController.java
+++ src/main/java/com/csemosip/bookingservice/controller/UserController.java
@@ -2,26 +2,40 @@ package com.csemosip.bookingservice.controller;
 
 
 import com.csemosip.bookingservice.dto.UserDTO;
+import com.csemosip.bookingservice.exception.ResourceNotFoundException;
 import com.csemosip.bookingservice.model.User;
+import com.csemosip.bookingservice.model.utils.Role;
 import com.csemosip.bookingservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
-public class UserController extends AbstractController{
+public class UserController extends AbstractController {
     @Autowired
     UserService userService;
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Map<String, Object>> findUser(@PathVariable String id) {
-        User user = userService.findUser(id);
+        User user;
+        try {
+            user = userService.findUser(id);
+        } catch (ResourceNotFoundException e) {
+            user = new User();
+            user.setUsername(id);
+            user.setRole(Role.RESOURCE_USER);
+        }
         return sendSuccessResponse(user, HttpStatus.OK);
     }
 
